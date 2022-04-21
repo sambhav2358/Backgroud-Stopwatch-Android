498b70196cfa92095a250d4013050e9806341322
sambhav2358 <uni.app.world@gmail.com>
2022-04-21 12:12:54 +0530

    Made changes

--- app/src/main/java/com/sambhav2358/testing/StopwatchNotificationActionReceiver.java
+++ app/src/main/java/com/sambhav2358/testing/StopwatchNotificationActionReceiver.java
@@ -26,22 +26,6 @@ public class StopwatchNotificationActionReceiver extends BroadcastReceiver {
                 PrefUtil.setIsRunningInBackground(context, true);
                 PrefUtil.setWasTimerRunning(context, true);
                 break;
-            case "r":
-                Log.v("actionClicked", "r");
-                
-                TimerService.timerHandler.removeCallbacks(TimerService.timerRunnable);
-                PrefUtil.setIsRunningInBackground(context, false);
-                PrefUtil.setTimerSecondsPassed(context, 0);
-                PrefUtil.setWasTimerRunning(context, false);
-                context.stopService(MainActivity.serviceIntent);
-
-                Intent intent1 = new Intent();
-                intent1.setClassName("com.sambhav2358.testing", "com.sambhav2358.testing.MainActivity");
-                intent1.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-
-                context.startActivity(intent1);
-                break;
-
         }
     }
 }
